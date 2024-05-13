4d41445648aedb0d8b04f9bf7a69b2dc35d113b1
LateThen <miscar@seznam.cz>
2024-05-13 18:06:20 +0200

    See post halfly done

--- bazarapp/src/pages/SeePost/SeePost.jsx
+++ bazarapp/src/pages/SeePost/SeePost.jsx
@@ -1,7 +1,21 @@
+import "SeePost.css"
 export default function SeePost(){
     return(
         <>
-        <h1>Info o postu</h1>
+        <div className="seepost-namecontainer">
+            Jméno:
+        </div>
+        <div className="seepost-descriptioncontainer">
+            Popis:
+        </div>
+        <div className="seepost-postname">
+            
+        </div>
+        <div className="seepost-horsepic">
+        </div>
+        <div className="seepost-saskadd2">Buy the Saskatchewan trip for 320 dollars right now!</div>
+        
         </>
     )
 }
+/*nastylovat atd*/
