9c816ac8bf10b3ea915b95ec29f99f34e2a5288f
LateThen <miscar@seznam.cz>
2024-05-13 15:17:00 +0200

    Loginpage halfly added

--- bazarapp/src/pages/Login/Login.jsx
+++ bazarapp/src/pages/Login/Login.jsx
@@ -1,7 +1,18 @@
+import "Login.css"
+import {Link} from "react-router-dom"
 export default function Login(){
     return(
         <>
-        <h1>Přihlásit se</h1>
+        
+        <div><input type="text" placeholder="E-mail"/></div>
+        <div><input type="text" placeholder="Heslo"/></div>
+        <button>Login</button>
+        <Link to="/register">
+            Don't have an account? Register now!
+        </Link>
         </>
     )
 }
+/*Přidat logiku pro prihlaseni */
+/*Přidat background img*/
+/*Přidat ten div z navbaru*/
