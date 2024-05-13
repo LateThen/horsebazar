07d529b59692580a05429f8cbf609fce7a8ade6f
LateThen <miscar@seznam.cz>
2024-05-13 14:40:47 +0200

    Made main page without styling

--- bazarapp/src/pages/MainPage/MainPage.jsx
+++ bazarapp/src/pages/MainPage/MainPage.jsx
@@ -1,8 +1,20 @@
+//nastyluj to pak vojtechu, pse jeden :3
+import "MainPage.css"
 export default function MainPage(){
     return(
         <>
         <h1>Hlavní stránka fr</h1>
-        <button>Log Out #2</button>
+        <div className="mainpage-containerofposts">
+            <div className="mainpage-specificpost">
+                <div className="mainpage-postname">Jméno inzerátu</div>
+                <div className="mainpage-postcost">Cena inzerátu</div>
+                <div className="mainpage-postimage">Obrázek inzerátu</div>
+            </div>
+        </div>
+        <div className="mainpage-saskadd1">11/10 Saska Recommend!</div>
+        
         </>
     )
+    /*image to be added*/ 
+    /*Přidat navbar!*/
 }
