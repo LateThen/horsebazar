3bf0c2f73093870109b9388e2f7fdeba72e2e2fa
LateThen <miscar@seznam.cz>
2024-05-13 17:42:27 +0200

    Register halfly added

--- bazarapp/src/pages/Register/Register.jsx
+++ bazarapp/src/pages/Register/Register.jsx
@@ -1,7 +1,18 @@
+import "Register.css"
+import {Link} from "react-router-dom"
 export default function Register(){
     return(
         <>
-        <h1>Registrovat</h1>
+        <div><input type="text" placeholder="E-mail"/></div>
+        <div><input type="text" placeholder="Heslo"/></div>
+        <div><input type="text" placeholder="Heslo znovu"/></div>
+        <button>Login</button>
+        <Link to="/login">
+           Have an account? Login now!
+        </Link>
         </>
     )
 }
+/*Přidat logiku pro registraci */
+/*Přidat background img*/
+/*Přidat ten div z navbaru*/
