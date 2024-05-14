f2ad3dc7bcfa4a953e404d11510403a8777bffc4
VojtaKdo <Atjov05@seznam.cz>
2024-05-14 09:25:22 +0200

    Work in progress styling NavBar

--- bazarapp/src/components/NavBar/NavBar.jsx
+++ bazarapp/src/components/NavBar/NavBar.jsx
@@ -3,26 +3,31 @@ import CreatePost from "../../pages/CreatePost/CreatePost";
 import MyPosts from "../../pages/MyPosts/MyPosts";
 import { memo } from "react";
 import "./NavBar.css";
+import 'bulma/css/bulma.css'
 
 export const NavBar = memo(function NavBar(props = {}) {
   return (
     <>
-      <div className="navbar">
+      <div className="has-background-grey-darker">
       <div className="navbar-imageLogo">
         <div className="navbar-title">
           THE HORSE BLACK MARKET
         </div>
-        </div>
-        <button className="navbar-buttonPosts">
-          <div className="navbar-posts">Inzeráty</div>
-        </button>
-        <button className="navbar-buttonCreatePosts">
-          <div className="navbar-createPosts">Vytvořit inzerát</div>
-        </button>
-        <button className="navbar-buttonMyPosts">
-          <div className="navbar-myPosts">Moje inzeráty</div>
-        </button>
       </div>
+
+        <button className="button is-large is-responsive has-background-grey-dark ml-5">
+          <div className="has-text-white-ter">Inzeráty</div>
+        </button>
+
+        <button className="button is-large is-responsive has-background-grey-dark ml-5">
+          <div className="has-text-white-ter">Vytvořit inzerát</div>
+        </button>
+
+        <button className="button is-large is-responsive has-background-grey-dark ml-5">
+          <div className="has-text-white-ter">Moje inzeráty</div>
+        </button>
+
+    </div>
     </>
   );
 });
