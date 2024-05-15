1612c5e812728b6a4f5672e1dffff3a3c4f206f8
LeDanos <daniel.nemec@student.spsmb.cz>
2024-05-15 14:36:18 +0200

    made the page work?

---
+++ bazarapp/src/models/Horse.ts
@@ -0,0 +1,13 @@
+
+  export type Horse = {
+    id?: string;
+    email?: string;
+    name?: string;
+    phonenumber?: number;
+    location?: string;
+    price?: number;
+    description?: string;
+    postname?: string;
+    //pridani obrazku
+  };
+  
--- bazarapp/src/pages/Register/Register.jsx
+++ bazarapp/src/pages/Register/Register.jsx
@@ -1,11 +1,38 @@
-import "Register.css"
-import {Link} from "react-router-dom"
+import "./Register.css"
+import {Link, useNavigate} from "react-router-dom"
+import React, {useState, useEffect} from "react";
+import {registerUser} from "../../models/User.js";
+
 export default function Register(){
+    const [formData, setFormData]=useState();
+    const [info, setInfo]=useState();
+    const navigate = useNavigate();
+
+    const handleChange = (e)=>{
+        setFormData({...formData, [e.target.name]: e.target.value});
+    }
+
+    const submit =async (e)=>{
+        e.preventDefault();
+        console.log(e)
+        const formDataToSend = new FormData();
+        for (const [key, value] of Object.entries(formData)) {
+            formDataToSend.append(key,value);
+        }
+        const upload = await postUpload(formDataToSend);
+        if (upload.status===201) return navigate("/");
+    }
+
+
     return(
         <>
-        <div><input type="text" placeholder="E-mail"/></div>
-        <div><input type="text" placeholder="Heslo"/></div>
-        <div><input type="text" placeholder="Heslo znovu"/></div>
+        <p>{info}</p>
+        <form encType="multipart/form-data">
+            <input type="text" name="email" placeholder="E-mail" onChange={handleChange} />
+            <input type="text" name="password" placeholder="Heslo" onChange={handleChange} />
+            <input type="text" name="passwordConfirm" placeholder="Heslo znovu" onChange={handleChange} />
+            <input type="submit" value={"submit"} onClick={submit} />
+        </form>
         <button>Login</button>
         <Link to="/login">
            Have an account? Login now!
