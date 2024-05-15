f74b1c27bf67f3b8d9fe8d9dbcab181d3bd67c67
VojtaKdo <Atjov05@seznam.cz>
2024-05-15 14:36:23 +0200

    NavBar Done, just need more styling

--- bazarapp/src/components/NavBar/NavBar.jsx
+++ bazarapp/src/components/NavBar/NavBar.jsx
@@ -8,23 +8,23 @@ import 'bulma/css/bulma.css'
 export const NavBar = memo(function NavBar(props = {}) {
   return (
     <>
-      <div className="has-background-grey-darker">
+    <div className="has-background-grey-darker is-flex p-5">
       <div className="navbar-imageLogo">
-        <div className="navbar-title">
+        <div className="navbar-title p-3">
           THE HORSE BLACK MARKET
         </div>
       </div>
-
-        <button className="button is-large is-responsive has-background-grey-dark ml-5">
-          <div className="has-text-white-ter">Inzeráty</div>
+ 
+        <button className="button is-large is-responsive has-background-grey-dark ml-5 has-text-white-ter is-size-3">
+          Inzeráty
         </button>
 
-        <button className="button is-large is-responsive has-background-grey-dark ml-5">
-          <div className="has-text-white-ter">Vytvořit inzerát</div>
+        <button className="button is-large is-responsive has-background-grey-dark ml-5 has-text-white-ter is-size-3">
+          Vytvořit inzerát
         </button>
 
-        <button className="button is-large is-responsive has-background-grey-dark ml-5">
-          <div className="has-text-white-ter">Moje inzeráty</div>
+        <button className="button is-large is-responsive has-background-grey-dark ml-5 has-text-white-ter is-size-3">
+          Moje inzeráty
         </button>
 
     </div>
--- bazarapp/src/pages/MainPage/MainPage.jsx
+++ bazarapp/src/pages/MainPage/MainPage.jsx
@@ -1,11 +1,15 @@
 import { NavBar } from "../../components/NavBar/NavBar"
-
+import "./MainPage.css"
 
 export default function MainPage(){
     return(
         <>
         <NavBar/>
-        <h1>Hlavní stránka fr</h1>
+        <div className="is-flex has-background-grey-dark has-text-white-ter mt-5 box">
+            <div className="ml-5">Name</div>
+            <div className="ml-5">Cost</div>
+            <img src="../../src/img/logOImage.png" alt="" className="ml-5"/>
+        </div>
         </>
     )
 }
