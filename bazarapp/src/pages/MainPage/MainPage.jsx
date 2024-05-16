63c4501d6b588c6b6c77c68d19ebdb41d11530ff
VojtaKdo <Atjov05@seznam.cz>
2024-05-16 23:22:10 +0200

    Navbar responsivity I fucking tried

--- bazarapp/src/pages/MainPage/MainPage.jsx
+++ bazarapp/src/pages/MainPage/MainPage.jsx
@@ -1,15 +1,9 @@
 import { NavBar } from "../../components/NavBar/NavBar"
-import "./MainPage.css"
 
 export default function MainPage(){
     return(
         <>
         <NavBar/>
-        <div className="is-flex has-background-grey-dark has-text-white-ter mt-5 box">
-            <div className="ml-5">Name</div>
-            <div className="ml-5">Cost</div>
-            <img src="../../src/img/logOImage.png" alt="" className="ml-5"/>
-        </div>
         </>
     )
 }
