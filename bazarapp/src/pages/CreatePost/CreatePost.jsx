a3ccddd62cbe2c5ce08f9ab4b75f05bc1e221d76
VojtaKdo <Atjov05@seznam.cz>
2024-05-18 17:35:23 +0200

    Pull request for NavBar

--- bazarapp/src/pages/CreatePost/CreatePost.jsx
+++ bazarapp/src/pages/CreatePost/CreatePost.jsx
@@ -1,6 +1,9 @@
+import { NavBar } from "../../components/NavBar/NavBar"
+
 export default function CreatePost(){
     return(
         <>
+        <NavBar/>
         <h1>Vytvořit inzerát</h1>
         </>
     )
