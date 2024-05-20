9e190d6b43cb7a5fde93ffaf1c66484c97fc3434
VojtaKdo <Atjov05@seznam.cz>
2024-05-20 23:26:30 +0200

    Upraveny barvy a responzivita, přídáno logo

--- bazarapp/src/components/NavBar/NavBar.jsx
+++ bazarapp/src/components/NavBar/NavBar.jsx
@@ -4,43 +4,38 @@ import MyPosts from "../../pages/MyPosts/MyPosts";
 import { memo } from "react";
 import { Link } from "react-router-dom";
 import "./NavBar.css";
-import 'bulma/css/bulma.css'
+import "bulma/css/bulma.css";
 
 export const NavBar = memo(function NavBar(props = {}) {
   return (
-  <>
-  <div className="navbar has-background-grey-darker is-flex p-5">
-    <div className="navbar-title-image-wrap">
-      <div className="navbar-imageLogo"></div>
-      <div className="navbar-title p-3">
-          THE HORSE BLACK MARKET
+    <>
+      <div className="navbar-all is-flex p-5">
+        <div className="navbar-title-image-wrap">
+          <img src="../../src/img/logoImage.png" className="navbar-imageLogo" />
+          <div className="navbar-title p-3">THE HORSE BLACK MARKET</div>
+        </div>
+
+        <div className="button-navbar-wrap">
+          <button className="button-navbar is-responsive">
+            <Link className="has-text-white-ter" to={"/"}>
+              Inzeráty
+            </Link>
+          </button>
+
+          <button className="button-navbar is-responsive">
+            <Link className="has-text-white-ter" to={"/createpost"}>
+              Vytvořit inzerát
+            </Link>
+          </button>
+
+          <button className="button-navbar is-responsive">
+            <Link className="has-text-white-ter" to={"/myposts"}>
+              Moje inzeráty
+            </Link>
+          </button>
+
+        </div>
       </div>
-      <div className="navbar-title-small p-3">
-          THBM
-  </div>
-    </div>
- 
-      <div className="button-navbar-wrap">
-        <button className="button-navbar is-responsive has-background-grey-dark">
-          <Link className="has-text-white-ter" to={"/"}>
-            Inzeráty
-          </Link>
-        </button>
-
-        <button className="button-navbar is-responsive has-background-grey-dark">
-          <Link className="has-text-white-ter" to={"/createpost"}>
-            Vytvořit inzerát
-          </Link>
-        </button>
-
-        <button className="button-navbar is-responsive has-background-grey-dark">
-          <Link className="has-text-white-ter" to={"/myposts"}>
-            Moje inzeráty
-          </Link>
-        </button>
-      </div>
-
-    </div>
     </>
   );
 });
