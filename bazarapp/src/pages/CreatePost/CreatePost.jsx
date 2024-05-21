1670ce0e43a5e9ddbc521669635e199ee060b311
VojtaKdo <Atjov05@seznam.cz>
2024-05-22 00:53:40 +0200

    I lost it, i don't know how to position anything, height: 100vh :,(

--- bazarapp/src/pages/CreatePost/CreatePost.jsx
+++ bazarapp/src/pages/CreatePost/CreatePost.jsx
@@ -5,21 +5,40 @@ export default function CreatePost(){
     return(
         <>
         <NavBar/>
-        <div className="saska-ad">Ever feel lonely? Do you have no one to talk to? Check out the saskadate.com site to find baddies in your area.</div>
-        <div className="is-flex is-flex-direction-column createpost-data">
-            <input type="text" className="createpost-username" placeholder="Jméno" required/>
-            <input type="tel" className="createpost-userphone" placeholder="Telefon" id="phone" name="phone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" required/>
-            <input type="text" className="createpost-useraddress" placeholder="Adresa" required/>
-            <input type="number" className="createpost-postcost" placeholder="Cena" min="0" max="10000" step="1" required/>
+        <div className="createpost is-flex">
+            <div className="createpost-saskadd2">
+                <button className="delete is-large popup-btn"></button>
+                Ever feel lonely? Do you have no one to talk to? Check out the saskadate.com site to find baddies in your area.
+            </div>
+
+        <div className="createpost-first-column is-flex is-flex-direction-column">
+            <div className="is-flex is-flex-direction-column createpost-data">
+                <input type="text" className="createpost-username" placeholder="Jméno" maxLength={"50"} required/>
+                <input type="tel" className="createpost-userphone" placeholder="Telefon 123-456-789" id="phone" name="phone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{3}" maxLength={"11"} required/>
+                <input type="text" className="createpost-useraddress" placeholder="Adresa" maxLength={"100"} required/>
+                <input type="number" className="createpost-postcost" placeholder="Cena" min="0" max="99999" step="1" required/>
+            </div>
+
+            <div className="createpost-descriptionbox is-flex">
+                <input type="text" className="createpost-description" placeholder="Poopis" required/>
+            </div>
+            
+            <input type="submit" className="createpost-button" value={"Vytvořit inzerát"} required/>
         </div>
-        <div className="createpost-descriptionbox is-flex">
-            <input type="text" className="createpost-description" placeholder="Poopis" required/>
+
+        <div className="is-flex is-flex-direction-column">
+
+        <form action="/action_page.php" className="uploadFile">
+            <input type="file" className="chooseFileInput" name="filename"/>
+            <input type="submit"/>
+        </form>
+
+            <div>
+                <input type="text" className="createpost-postname" placeholder="Název inzerátu" required/>
+            </div>
         </div>
-        <div>
-            <input type="text" className="createpost-postname" placeholder="Název inzerátu" required/>
+            
         </div>
-        <input type="submit" className="createpost-button" value={"Vytvořit inzerát"} required/>
-        <img src="../../src/img/logoImage.png" alt="" className="createpost-postimage"/>
         </>
     )
 }
