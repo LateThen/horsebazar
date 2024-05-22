99fbc45523e4e8b1fa12e3ab41d6c5f7fe71788f
LeDanos <daniel.nemec@student.spsmb.cz>
2024-05-22 14:24:22 +0200

    man, fuck bulma

--- bazarapp/src/pages/Register/Register.jsx
+++ bazarapp/src/pages/Register/Register.jsx
@@ -6,21 +6,23 @@ export default function Register(){
 
     return(
         <>
-        <img src="../../src/img/logoImage.png" alt="" id="image"/>
-        <div id="content">
-            <form encType="multipart/form-data" id="form">
-                <input type="text" name="email" placeholder="E-mail"/>
-                <input type="text" name="password" placeholder="Heslo"/>
-                <input type="text" name="passwordConfirm" placeholder="Heslo znovu"/>
-                <input type="submit" value={"login"}/>
-            </form>
-            <Link to="/login">
-            Have an account? Login now!
-            </Link>
-        </div>
+            <div className="is-flex is-flex-direction-row margin-0">
+                <img src="../../src/img/bg.png" alt="background image" className="bg p-0 column"/>
+                <div className="content p-0 has-text-centered column"> 
+                    <img src="../../src/img/logoImage.png" alt="logo" />
+                    <form encType="multipart/form-data" className="form is-flex-direction-column is-flex">
+                        <input type="text" name="email" placeholder="E-mail"/>
+                        <input type="text" name="password" placeholder="Heslo"/>
+                        <input type="text" name="passwordConfirm" placeholder="Heslo znovu"/>
+                        <input type="submit" value={"login"}/>
+                    </form>
+                    <Link to="/login">
+                    Have an account? Login now!
+                    </Link>
+                </div>
+            </div>
         </>
     )
 }
 /*Přidat logiku pro registraci */
-/*Přidat background img*/
 /*Přidat ten div z navbaru*/
