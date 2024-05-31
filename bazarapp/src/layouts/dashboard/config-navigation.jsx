991d08ba8e5c539c1bc231a00d7b1bdcce809361
Michal Kostecký <misa.kostecky@seznam.cz>
2024-05-31 12:01:27 +0200

    registerdole

--- bazarapp/src/layouts/dashboard/config-navigation.jsx
+++ bazarapp/src/layouts/dashboard/config-navigation.jsx
@@ -1,41 +1,44 @@
-import SvgColor from '../../components/svg-color';
+import SvgColor from "../../components/svg-color";
 
 // ----------------------------------------------------------------------
 
 const icon = (name) => (
-  <SvgColor src={`/assets/icons/navbar/${name}.svg`} sx={{ width: 1, height: 1 }} />
+  <SvgColor
+    src={`/assets/icons/navbar/${name}.svg`}
+    sx={{ width: 1, height: 1 }}
+  />
 );
 
 const navConfig = [
   {
-    title: 'Buy Horses',
-    path: '/products',
-    icon: icon('ic_analytics'),
+    title: "Buy Horses",
+    path: "/products",
+    icon: icon("ic_analytics"),
   },
   {
-    title: 'My Posts',
-    path: '/mypost',
-    icon: icon('ic_user'),
+    title: "My Posts",
+    path: "/mypost",
+    icon: icon("ic_user"),
   },
   {
-    title: 'Create Post',
-    path: '/createpost',
-    icon: icon('ic_cart'),
+    title: "Create Post",
+    path: "/createpost",
+    icon: icon("ic_cart"),
   },
 
   {
-    title: 'login',
-    path: '/login',
-    icon: icon('ic_lock'),
+    title: "login",
+    path: "/login",
+    align: true,
+    icon: icon("ic_lock"),
   },
   {
-  title: 'register',
-  path: '/register',
-  icon: icon('ic_lock'),
-  className: 'config-navigation-register-item',
-},
-
-
+    title: "register",
+    align: true,
+    path: "/register",
+    icon: icon("ic_lock"),
+    className: "config-navigation-register-item",
+  },
 ];
 
 export default navConfig;
