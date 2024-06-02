9191aeecea348b2fcfe2b33e113f476093ac90ca
LateThen <miscar@seznam.cz>
2024-06-02 13:06:33 +0200

    changes for fullserver branch

--- bazarapp/src/models/Horse.ts
+++ bazarapp/src/models/Horse.ts
@@ -1,4 +1,31 @@
+export const createHorse = async (formData) => {
+  const res = await fetch("http://localhost:3000/api/v1/horse", {
+    body: formData,
+    method: "POST",
+  });
+  const data = await res.json();
+  return {
+    status: res.status,
+    msg: data.msg,
+  };
+};
 
+export const getHorses = async () => {
+  const req = await fetch("http://localhost:3000/api/v1/horse", {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    method: "GET",
+  });
+  const data = await req.json();
+  return {
+    status: req.status,
+    payload: data.payload,
+    msg: data.msg,
+  };
+};
+  
   export type Horse = {
     id?: string;
     name?: string;
